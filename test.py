0c1c841fc7edf4f2d48630d3a859dd8e2c70523f
Mako <mnfeconicu41@gmail.com>
2018-07-11 18:34:54 +0900

    python test.py: add verbosity=1 mode

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 
+import sys
 import unittest
 
+verbosity = 2
+if len(sys.argv) >= 2 and sys.argv[1].startswith('-'):
+    verbosity = 1
+
 suite = unittest.TestLoader().discover(start_dir='tests', pattern='test_*.py')
-unittest.TextTestRunner(verbosity=2).run(suite)
+unittest.TextTestRunner(verbosity=verbosity).run(suite)
