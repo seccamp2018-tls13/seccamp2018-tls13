04f617f5de909752ab339adfad2f74984924935c
ykm11 <ushiromiya3@gmail.com>
2018-08-03 00:21:04 +0900

    update tests/.../test_chacha20poly1305.py for AEAD test

--- tests/test_utils/test_encryption/test_chacha20poly1305.py
+++ tests/test_utils/test_encryption/test_chacha20poly1305.py
@@ -16,6 +16,10 @@ class Chacha20Poly1305Test(unittest.TestCase):
             '1b1589a4aa99de2b51267ad8bee329d3eb672e88bda24b31f29ad8405dfe2e1c')
         self.nonce2 = binascii.unhexlify(
             '5c2bd761df1669f08b26ddb6ef7139e09e898d48593591da89d0357095de0e5c')
+        self.auth_data = binascii.unhexlify(
+            'd761d8b26ddb629ad8405ef713e09e89e4584d4c08f6e3c850b2f29ad8405976')
+        self.auth_data2 = binascii.unhexlify(
+            '26cb24b3db2329df29ad0d0eedeadbeef7dda2c850b2f59ef715126789a4efea')
 
     def test_enc_dec(self):
         polychacha = Chacha20Poly1305(self.key, self.nonce)
@@ -36,3 +40,11 @@ class Chacha20Poly1305Test(unittest.TestCase):
         enc = polychacha.encrypt(self.plain)
         dec = polychacha2.decrypt(enc)
         self.assertNotEqual(self.plain, dec)
+
+    def test_aued(self):
+        polychacha = Chacha20Poly1305(self.key, self.nonce)
+        enc, tag = polychacha.chacha20_aead_encrypt(self.auth_data, self.plain)
+        enc2, tag2 = polychacha.chacha20_aead_encrypt(self.auth_data2, self.plain)
+        self.assertEqual(enc, enc2)
+        self.assertNotEqual(tag, tag2)
+
