635292d5b0cf871ce0408b78b2173696719e56d6
Mako <mnfeconicu41@gmail.com>
2018-12-16 18:16:07 +0900

    create Connection class

--- tls13/utils/connection.py
+++ tls13/utils/connection.py
@@ -10,41 +10,31 @@ import socket
 HOST = 'localhost' # The remote host
 PORT = 50007
 
-class ClientConnection:
+class Connection:
+    def send_msg(self, byte_str):
+        self.socket.sendall(byte_str)
+
+    def recv_msg(self):
+        # NOTE: ハードウェアおよびネットワークの現実に最大限マッチするように、
+        #       bufsize の値は比較的小さい2の累乗、たとえば 4096、にすべきです。
+        return self.socket.recv(2**14) # TLSPlaintext の最大の大きさが 2^14 byte
+
+
+class ClientConnection(Connection):
     def __init__(self, host=HOST, port=PORT):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-
-    def send_msg(self, byte_str):
-        self.sock.sendall(byte_str)
-
-    def recv_msg(self):
-        # NOTE: ハードウェアおよびネットワークの現実に最大限マッチするように、
-        #       bufsize の値は比較的小さい2の累乗、たとえば 4096、にすべきです。
-        data = self.sock.recv(2**14) # TLSPlaintext の最大の大きさが 2^14 byte
-        return data
+        self.socket = self.sock
 
 
-class ServerConnection:
-    def __init__(self, host=HOST, port=PORT, _socket=None):
-        if _socket:
-            self.sock = _socket
-        else:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # prevent "Address already in use" error
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.sock.bind((host, port))
-            self.sock.listen(1)
+class ServerConnection(Connection):
+    def __init__(self, host=HOST, port=PORT):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # prevent "Address already in use" error
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((host, port))
+        self.sock.listen(1)
         conn, addr = self.sock.accept()
-        self.conn = conn
+        self.socket = conn
         self.addr = addr
         print('Connected by', self.addr)
-
-    def send_msg(self, byte_str):
-        self.conn.sendall(byte_str)
-
-    def recv_msg(self):
-        # NOTE: ハードウェアおよびネットワークの現実に最大限マッチするように、
-        #       bufsize の値は比較的小さい2の累乗、たとえば 4096、にすべきです。
-        data = self.conn.recv(2**14) # TLSPlaintext の最大の大きさが 2^14 byte
-        return data
